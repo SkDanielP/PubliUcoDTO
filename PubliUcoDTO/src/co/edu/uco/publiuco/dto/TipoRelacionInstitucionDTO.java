89fe62faba18a364dac01eb5e1e689cea5b028fe
Daniel <danielprdj@gmail.com>
2023-04-26 15:22:22 -0500

    commit 3

--- PubliUcoDTO/src/co/edu/uco/publiuco/dto/TipoRelacionInstitucionDTO.java
+++ PubliUcoDTO/src/co/edu/uco/publiuco/dto/TipoRelacionInstitucionDTO.java
@@ -2,30 +2,34 @@ package co.edu.uco.publiuco.dto;
 
 import java.util.UUID;
 
+import co.edu.uco.publiuco.crosscutting.utils.UtilObject;
 import co.edu.uco.publiuco.crosscutting.utils.UtilText;
 import co.edu.uco.publiuco.crosscutting.utils.UtilUUID;
 
 
-public class EstadoTipoRelacionInstitucionDTO {
+public class TipoRelacionInstitucionDTO {
 	private UUID identificador;
 	private String nombre;
 	private String descripcion;
+	private EstadoTipoRelacionInstitucionDTO estado;
 	
 	
 	
-	public EstadoTipoRelacionInstitucionDTO(UUID identificador, String nombre, String descripcion) {
+	public TipoRelacionInstitucionDTO(final UUID identificador,final String nombre,final String descripcion, final EstadoTipoRelacionInstitucionDTO estado) {
 		super();
 		setIdentificador(identificador);
 		setNombre(nombre);
 		setDescripcion(descripcion);	
+		setEstado(estado);
 		}
 	
 	
-	public EstadoTipoRelacionInstitucionDTO() {
+	public TipoRelacionInstitucionDTO() {
 		super();
 		setIdentificador(UtilUUID.DEFAULT_UUID);
 		setNombre(UtilText.getUtilText().getDefaultValue());
 		setDescripcion(UtilText.EMPTY);
+		setEstado(EstadoTipoRelacionInstitucionDTO.create());
 	}
 	
 	public static EstadoTipoRelacionInstitucionDTO create() {
@@ -35,7 +39,7 @@ public class EstadoTipoRelacionInstitucionDTO {
 	public final UUID getIdentificador() {
 		return identificador;
 	}
-	public final EstadoTipoRelacionInstitucionDTO setIdentificador(final UUID identificador) {
+	public final TipoRelacionInstitucionDTO setIdentificador(final UUID identificador) {
 		this.identificador = UtilUUID.getDefault(identificador); 
 		
 		return this;
@@ -45,7 +49,7 @@ public class EstadoTipoRelacionInstitucionDTO {
 	}
 	
 	
-	public final EstadoTipoRelacionInstitucionDTO setNombre(final String nombre) {
+	public final TipoRelacionInstitucionDTO setNombre(final String nombre) {
 		this.nombre = UtilText.getUtilText().applyTrim(nombre);
 		return this;
 		
@@ -56,11 +60,24 @@ public class EstadoTipoRelacionInstitucionDTO {
 	}
 	
 	
-	public final EstadoTipoRelacionInstitucionDTO setDescripcion(final String descripcion) {
+	public final TipoRelacionInstitucionDTO setDescripcion(final String descripcion) {
 		this.descripcion = UtilText.getUtilText().applyTrim(nombre);
 		
 		return this;
 	}
+
+
+	public final EstadoTipoRelacionInstitucionDTO getEstado() {
+		return estado;
+	}
+
+
+	public final TipoRelacionInstitucionDTO setEstado(final EstadoTipoRelacionInstitucionDTO estado) {
+		this.estado = UtilObject.getDefault(estado, EstadoTipoRelacionInstitucionDTO.create());
+		return this;
+	}
+	
+	
 
 
 	
